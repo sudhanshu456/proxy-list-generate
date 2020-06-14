2d523a91e8f084bbaed8d73284df51e36577e4e3
sudhanshu456 <spsupraj2d@gmail.com>
2020-06-14 23:38:12 +0530

    coverting into functions

--- sel.py
+++ sel.py
@@ -1,6 +1,8 @@
 import time
 import json
 import os
+from threading import thread
+import concurrent.futures
 from datetime import datetime,timedelta
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
@@ -8,18 +10,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+chromedriver=os.path.join(os.getcwd(),'chromedriver','chromedriver.exe')
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument("start-maximized")
+options.add_argument("disable-infobars")
+options.add_argument('--disable-dev-shm-usage')
 
-def scraper():
-    chromedriver=os.path.join(os.getcwd(),'chromedriver','chromedriver.exe')
-    options = webdriver.ChromeOptions()
+
+def scrapper1():
     #options.add_argument("user-data-dir=C:\\Users\\ASUS\\AppData\\Local\\Google\\Chrome\\User Data\\")
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument("start-maximized")
-    options.add_argument("disable-infobars")
-    options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(executable_path=chromedriver,options =options)
-
     driver.get("http://spys.one/en/anonymous-proxy-list/")
     select=Select(driver.find_element_by_id('xpp'))
     select.select_by_value('5')
@@ -30,7 +32,16 @@ def scraper():
     proxlis=set()
     for i in table:
         proxlis.add(i[0])
-        
+    driver.quit()
+    try :
+        proxlis.remove('Proxy address:port')
+    except:
+        pass
+    return list(proxlis),"spys"
+
+def scrapper2():
+    proxlis=set()
+    driver = webdriver.Chrome(executable_path=chromedriver,options =options)
     for i in range(1,6):
         
         driver.get("http://free-proxy.cz/en/proxylist/country/all/https/date/all/{}".format(i))
@@ -47,9 +58,14 @@ def scraper():
         z=driver.find_element_by_id('zkzk').text
         for k in z.split('\n'):
             proxlis.add(k)
+    driver.quit()
+    return list(proxlis),"free_proxy.cz"
 
+def scrapper3():
+    proxlis=set()
+    driver = webdriver.Chrome(executable_path=chromedriver,options =options)
     driver.get("https://free-proxy-list.net/")
-
+    proxlis=set()
     for k in range(0,15):
             
         table=[[col.text for col in i.find_elements_by_tag_name('td')] for i in driver.find_elements_by_xpath('//*[@id="proxylisttable"]/tbody/tr')]
@@ -58,7 +74,12 @@ def scraper():
                 proxlis.add(z)
                 
         driver.find_element_by_xpath('/html/body/section[1]/div/div[2]/div/div[3]/div[2]/div/ul/li[10]/a').click()
+    driver.quit()
+    return list(proxlis),"free_prozy_list.net"
 
+def scrapper4():
+    proxlis=set()
+    driver = webdriver.Chrome(executable_path=chromedriver,options =options)    
     driver.get('http://nntime.com/')
     driver.find_element_by_xpath('//*[@id="formname"]/div[1]/a[1]').click()
     e=driver.find_element_by_xpath('//*[@id="form1"]/div[1]/textarea')
@@ -72,14 +93,8 @@ def scraper():
         for j in e.get_attribute("value").split("\n"):
             proxlis.add(j)
     driver.quit()
-
     # read the list of proxy IPs in proxyList
-    try :
-        proxlis.remove('Proxy address:port')
-    except:
-        pass
-
-    return list(proxlis)
+    return list(proxlis),"nntime.com"
 
 def is_bad_proxy(pip):
     import urllib.request , socket
@@ -110,9 +125,12 @@ def check_create_file(proxyList):
         else:
             print (item, "is working")
             working.append(item)
-    date=(datetime.now()+timedelta(30)).strftime("%d_%m_%Y")
+    return working
+    
 
-    filename='./output/file_{}.txt'.format(date)
+def write_file(url,working):
+    date=(datetime.now()+timedelta(30)).strftime("%d_%m_%Y")
+    filename='./output/file{url}_{date}.txt'.format(url=url,date=date)
     file=open(filename,'w')
     for i in working:
         file.writelines(i+"\n")
@@ -120,5 +138,7 @@ def check_create_file(proxyList):
     file.close()
 
 if __name__ == '__main__':
-    proxli=scraper()
-    check_create_file(proxli)
+    x,y=scrapper1()
+    x1,y1=scrapper2()
+    x2,y2=scrapper3()
+    x3,y3=scrapper4()
